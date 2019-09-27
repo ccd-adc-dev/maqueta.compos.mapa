8b80eb5d251ae87dd5ce758462b6ccd1db39be11
MitziYolotzin <mitziulter@gmail.com>
2019-09-27 14:16:40 -0500

    estilos ventana elementos

--- src/componentes/VentanaFamilia/VentanaFamilia.svelte
+++ src/componentes/VentanaFamilia/VentanaFamilia.svelte
@@ -23,9 +23,14 @@
         justify-content: space-between;
     }
 
+    .Cerrar {
+        left: 285px;
+        position: relative;
+    }
+
     img {
-		width: 100%;
-		height: 12rem;
+		width: 580px;
+		height: 455px;
         object-fit: cover;
         box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
         border-radius: 5px;
@@ -33,10 +38,21 @@
     
     .Titulo {
         text-align: center;
+        font-weight: 200;
+        font-size: 24px;
+        line-height: 29px;
+        letter-spacing: 0.2em;
+        color: #454344;
     }
 
     .Principal {
         text-align: center;
+        font-weight: 600;
+        font-size: 44px;
+        line-height: 53px;
+        text-align: center;
+        color: #E6AA30;
+        padding: 0;
     }
 
     .Saber {
@@ -45,7 +61,11 @@
         border: 1px solid #E6AA30;
         box-sizing: border-box;
         border-radius: 15px;
+        left: 5px;
+        position: relative;
+        color: #E6AA30;
     }
+    
 
     /* .Cerrar {
         height:20px;
@@ -63,29 +83,44 @@
         margin: 1em;
     }
 
+    .TextoFotos {
+        position: relative;
+        top: -102px;
+        left: -220px;
+        color: #fff;
+    }
+
+    .Contenedor-1 {
+        background: #F7F7F7;
+    }
+
 </style>
 
 <article class="Familia">
-    <BotonCerrar/>
+    <div class="Cerrar">
+        <BotonCerrar/>
+    </div>
     <header>
         <p class="Titulo">
             Familia
         </p>
         <h1 class="Principal">
-            Yuto-Nahua
+            Yuto-nahua
         </h1>
-        <button class="Saber">
-            Saber mas
-        </button>
     </header>
+        <button class="Saber">
+            Saber más
+        </button>
          <!-- <img src="http://fakeimg.pl/244x80?text=logo" alt="img" /> -->
 	<img class="Imagen" src={`http://unsplash.it/300/${100+Math.floor(Math.random()*500)}`} alt="img"/>
-        <p>
-            Titulo Foto
-        </p>
-        <p>
-            Descripcion
-        </p>
+        <div class="TextoFotos">
+            <p>
+                Titulo Foto
+            </p>
+            <p>
+                Descripción
+            </p>
+        </div>
 </article>
 
 <div class="Slider">
@@ -95,12 +130,12 @@
 
 <article class="Contenedor-1">
 <header class="TitulosLista">
-    <h5>
-        Agrupaciones Linguisticas
-    </h5>
-    <h5>
-        Riesgo de desaparicion
-    </h5>
+    <h4>
+        Agrupaciones Lingüísticas
+    </h4>
+    <h4>
+        Riesgo de desaparición
+    </h4>
 </header>
     <section>
         <ul>
