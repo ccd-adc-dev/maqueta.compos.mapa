fa6c5770c41e0f45a02783bca8c0f02b8d934758
MitziYolotzin <mitziulter@gmail.com>
2019-09-30 20:33:54 -0500

    componentes en fila

--- src/App.svelte
+++ src/App.svelte
@@ -8,20 +8,58 @@
 </script>
 
 <style>
+	* {
+		outline: 1px solid blue;
+	}
 	
+	.Contenedor-1 {
+		width: 100%;
+		padding: 0;
+		margin: 0;
+		display: flex;
+		flex-flow: row wrap;
+		justify-content: space-around;
+
+  		-webkit-flex-display: flex;
+	    display: flex;
+  		-webkit-flex-flow: row wrap;
+	}
+
+	.VentanaFamilia {
+		padding: 1rem;
+  		width: 360px;
+  		height: 420px;
+  		margin-top: 1rem;
+	}
+
+	.Informacion {
+		padding: 1rem;
+  		width: 360px;
+  		height: 420px;
+  		margin-top: 1rem;
+	}
+
+
 </style>
 
 <head>
     <script src="https://kit.fontawesome.com/096c98e281.js" crossorigin="anonymous"></script>
 </head>
+<section class="Contenedor-1">
+		<div class="VentanaFamilia">
+			<VentanaFamilia/>
+		</div>
+		<div class="Informacion">
+			<Informacion/>
+		</div>
+		<div class="IconoVoluta">
+			<VolutaIcono/>
+		</div>
+		<div class="IconoTwitter">
+			<TwitterIcono/>
+		</div>
 
-<VentanaFamilia/>
-<br>
-<Informacion/>
-
-<VolutaIcono/>
-<TwitterIcono/>
-
+</section>
 
 
 
